91ac46ca719ab4c4e5742ec9997b7afb85a8f48c
Muhamad Irsal Firansyah <muhamadirsal@gmail.com>
2021-07-20 14:43:17 +0700

    refactor function initialize db

--- app/routes.go
+++ app/routes.go
@@ -1,7 +1,11 @@
 package app
 
-import "github.com/irsalsss/gotoko/app/controllers"
+import (
+	"github.com/gorilla/mux"
+	"github.com/irsalsss/gotoko/app/controllers"
+)
 
 func (server *Server) InitializeRoutes() {
+	server.Router = mux.NewRouter()
 	server.Router.HandleFunc("/", controllers.Home).Methods("GET")
 }
--- app/server.go
+++ app/server.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -22,11 +23,30 @@ type AppConfig struct {
 	AppPort string
 }
 
-func (server *Server) Initialize(appConfig AppConfig) {
+type DBConfig struct {
+	DBHost     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	DBPort     string
+}
+
+func (server *Server) Initialize(appConfig AppConfig, dbConfig DBConfig) {
 	fmt.Println("Welcome to " + appConfig.AppName)
 
-	server.Router = mux.NewRouter()
+	server.initializeDB(dbConfig)
 	server.InitializeRoutes()
+}
+
+func (server *Server) initializeDB(dbConfig DBConfig) {
+	var err error
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta", dbConfig.DBHost, dbConfig.DBUser, dbConfig.DBPassword, dbConfig.DBName, dbConfig.DBPort)
+
+	server.DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("Failed on connecting to database server")
+	}
 }
 
 func (server *Server) Run(addr string) {
@@ -45,6 +65,7 @@ func getEnv(key, fallback string) string {
 func Run() {
 	var server = Server{}
 	var appConfig = AppConfig{}
+	var dbConfig = DBConfig{}
 
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error on loading .env file")
@@ -54,6 +75,12 @@ func Run() {
 	appConfig.AppEnv = getEnv("APP_ENV", "development")
 	appConfig.AppPort = getEnv("APP_PORT", "9000")
 
-	server.Initialize(appConfig)
+	dbConfig.DBHost = getEnv("DB_HOST", "localhost")
+	dbConfig.DBName = getEnv("DB_NAME", "gotoko")
+	dbConfig.DBUser = getEnv("DB_USER", "irsal")
+	dbConfig.DBPassword = getEnv("DB_PASSWORD", "password")
+	dbConfig.DBPort = getEnv("DB_PORT", "5432")
+
+	server.Initialize(appConfig, dbConfig)
 	server.Run(":" + appConfig.AppPort)
 }
